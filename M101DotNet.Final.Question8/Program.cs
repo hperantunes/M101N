7d05c0fdcbf7e7f5004f9923f1c57c7cc3a1d62c
Hilton Perantunes <hperantunes@gmail.com>
2015-05-03 21:53:45 -0400

    Question code

---
+++ M101DotNet.Final.Question8/Program.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace M101DotNet.Final.Question8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var client = new MongoClient();
+            var db = client.GetDatabase("test");
+
+            var animals = db.GetCollection<BsonDocument>("animals");
+
+            var animal = new BsonDocument { 
+                {"animal", "monkey"}
+            };
+            await animals.InsertOneAsync(animal);
+            
+            animal.Remove("animal");
+            animal.Add("animal", "cat");
+            animal["_id"] = ObjectId.GenerateNewId();
+            await animals.InsertOneAsync(animal);
+            
+            animal.Remove("animal");
+            animal.Add("animal", "lion");
+            animal["_id"] = ObjectId.GenerateNewId();
+            await animals.InsertOneAsync(animal);
+        }
+    }
+}
